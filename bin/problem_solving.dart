178a50cb7e762983f625d2ce667465facdf2f305
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2023-07-17 19:56:08 +0300

    very big numbers sum solve
    __

--- bin/problem_solving.dart
+++ bin/problem_solving.dart
@@ -2,10 +2,19 @@
 
 import 'array_sum/array_sum.dart';
 import 'problem2.dart';
+import 'dart:core';
+
+import 'very_big_sum/very_big.dart';
 
 void main(List<String> arguments) {
-  print(
-    ArraySum.printSum([3, 1000, 4]),
-  );
+  int x = 55;
+  print(x.toString().length);
+  print("the very big result is ${VeryBigArraySum.aVeryBigSumMethod([
+        BigInt.parse("5000000000000000000000"),
+        BigInt.parse("5000000000000000000000"),
+        BigInt.parse("5000000000000000000000"),
+        BigInt.parse("5000000000000000000000"),
+      ])}");
+  print(ArraySum.printSum([3, 1000, 4]));
   print(ProblemBSolution.compareTriplets([4, 3, 3], [3, 2, 1]));
 }
