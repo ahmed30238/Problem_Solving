07f23799d5bb95b91e6a4f44c76c51b5a511d141
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2023-12-29 12:05:46 +0200

    socket merchant problem

---
+++ bin/socket_merchant/socket_merchant.dart
@@ -0,0 +1,14 @@
+//
+void main(List<String> args) {
+  SocketMerchant.sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]);
+}
+
+class SocketMerchant {
+  static int sockMerchant(int n, List<int> ar) {
+    int pairsNumber = 0;
+    List<int> possiblePairs = [];
+
+    print(possiblePairs);
+    return pairsNumber;
+  }
+}
