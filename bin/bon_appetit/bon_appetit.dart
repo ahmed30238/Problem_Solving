9886a74f310b4ce4f2549554d955765c482f8d63
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2023-12-29 11:11:13 +0200

    bon appetit problem

---
+++ bin/bon_appetit/bon_appetit.dart
@@ -0,0 +1,16 @@
+//
+void main(List<String> args) {
+  BonAppetit.bonAppetit([3, 10, 2, 9], 1, 12);
+}
+
+class BonAppetit {
+  static void bonAppetit(List<int> bill, int k, int b) {}
+}
+
+/**
+ * bonAppetit has the following parameter(s):
+
+bill: an array of integers representing the cost of each item ordered
+k: an integer representing the zero-based index of the item Anna doesn't eat
+b: the amount of money that Anna contributed to the bill
+ */
