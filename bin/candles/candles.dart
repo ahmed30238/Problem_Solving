74e3b8a45fe41c2190e8e4869fc05bedba969ead
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2023-12-31 11:08:10 +0200

    another solution for birthday candles problem

--- bin/candles/candles.dart
+++ bin/candles/candles.dart
@@ -1,36 +1,49 @@
 // ignore: slash_for_doc_comments
+import 'package:quick_log/quick_log.dart';
+
 /**
  you have list of integer that you need to find the tallest number and how many of them then retun the number
  ex => [4, 4, 1, 3]the maximum height candles are 4 units high, there are two of them, so return 2.
  */
+void main(List<String> args) {
+  print(BirthdayCandles.birthDayCandles([44, 53, 31, 27, 77, 60, 66, 77, 26, 36]));
+}
+
 class BirthdayCandles {
-  int birthDayCandles(List<int> arr) {
-    List<int> tallestCandles = [];
-    arr.sort();
-    // for (var i = arr.length - 1; i > 0; i--) {
-    //   if (arr.last > arr.length - 1) {
-    //     tallestCandles.add(arr.last);
-    //     break;
-    //   } else if (arr[i] == arr[i - 1]) {
-    //     tallestCandles.add(arr[i]);
-    //   }
-    // }
-    for (var i = arr.length - 1; i > 0;) {
-      if (arr[i] > arr[i - 1]) {
-        tallestCandles.add(arr[i]);
-      } else {
-        for (var m = i; m > 0;) {
-          for (var j = m - 1; j >= 0; j--) {
-            if (arr[m] == arr[j]) {
-              tallestCandles.add(arr[j]);
-            }
-          }
-          tallestCandles.add(arr[m]);
-          break;
-        }
-      }
-      break;
+  static int birthDayCandles(List<int> arr) {
+    // This is another solution
+    Map<int, int> candles = {};
+    int theValue = 0;
+    var theKey = 0;
+    for (var element in arr) {
+      candles.update(element, (value) => value + 1, ifAbsent: () => 1);
     }
-    return tallestCandles.length;
+
+    candles.forEach((key, value) {
+      if (value > theValue) {
+        theValue = value;
+        theKey = key;
+      }
+    });
+    return theValue;
+    // List<int> tallestCandles = [];
+    // arr.sort();
+    // for (var i = arr.length - 1; i > 0;) {
+    //   if (arr[i] > arr[i - 1]) {
+    //     tallestCandles.add(arr[i]);
+    //   } else {
+    //     for (var m = i; m > 0;) {
+    //       for (var j = m - 1; j >= 0; j--) {
+    //         if (arr[m] == arr[j]) {
+    //           tallestCandles.add(arr[j]);
+    //         }
+    //       }
+    //       tallestCandles.add(arr[m]);
+    //       break;
+    //     }
+    //   }
+    //   break;
+    // }
+    // return tallestCandles.length;
   }
 }
