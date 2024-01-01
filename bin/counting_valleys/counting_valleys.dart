65fec3fd8c9f3b36b59032ce8ade586d4e0aaede
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-01 14:45:35 +0200

    counting valleys solution

--- bin/counting_valleys/counting_valleys.dart
+++ bin/counting_valleys/counting_valleys.dart
@@ -1,10 +1,24 @@
 //
 void main(List<String> args) {
-  CountingValleys.countingValleys(8, "UDDDUDUU");
+  print(CountingValleys.countingValleys(8, "UDDDUDUU"));
 }
 
 class CountingValleys {
   static int countingValleys(int steps, String path) {
-    return 0;
+    int seaLevel = 0;
+    int valleysCounter = 0;
+
+    for (var i = 0; i < path.length; i++) {
+      if (path[i] == "D") {
+        seaLevel -= 1;
+      } else if (path[i] == "U") {
+        seaLevel += 1;
+      }
+      print("cLevel $seaLevel");
+      if (seaLevel == -1 && i < path.length - 1 && path[i + 1] == "U") {
+        valleysCounter++;
+      }
+    }
+    return valleysCounter;
   }
 }
