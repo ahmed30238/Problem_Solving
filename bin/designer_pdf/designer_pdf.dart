41eb4f5df563baae1361651b3d7221ec9eac12da
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-05 12:19:05 +0200

    designer pdf another solution

--- bin/designer_pdf/designer_pdf.dart
+++ bin/designer_pdf/designer_pdf.dart
@@ -1,36 +1,76 @@
 //
 
 void main(List<String> args) {
-  print(DesignerPdf.designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], "abc"));
+  print(DesignerPdf.designerPdfViewer([
+    1,
+    3,
+    1,
+    3,
+    1,
+    4,
+    1,
+    3,
+    2,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5
+  ], "abc"));
 }
 
 class DesignerPdf {
   static int designerPdfViewer(List<int> h, String word) {
-    var aCode = 'a'.codeUnitAt(0);
-    var zCode = 'z'.codeUnitAt(0);
-    List<String> alphabets = List<String>.generate(
-      zCode - aCode + 1,
-      (index) => String.fromCharCode(aCode + index),
-    );
-    var wordInLetters = word.split("");
-    int maxHeight = 1;
+    // var aCode = 'a'.codeUnitAt(0);
+    // var zCode = 'z'.codeUnitAt(0);
 
-    List<int> indexes = [];
-    for (var i = 0; i < wordInLetters.length; i++) {
-      indexes.add(alphabets.indexOf(wordInLetters[i]));
+    // List<String> alphabets = List<String>.generate(
+    //   zCode - aCode + 1,
+    //   (index) => String.fromCharCode(aCode + index),
+    // );
+    // var wordInLetters = word.split("");
+    // int maxHeight = 1;
+
+    // List<int> indexes = [];
+    // for (var i = 0; i < wordInLetters.length; i++) {
+    //   indexes.add(alphabets.indexOf(wordInLetters[i]));
+    // }
+    // var heights = [];
+    // for (var i = 0; i < indexes.length; i++) {
+    //   heights.add(h[indexes[i]]);
+    // }
+    // maxHeight = heights.reduce((curr, next) {
+    //   if (curr > next) {
+    //     return curr;
+    //   } else {
+    //     return next;
+    //   }
+    // });
+    // int highlightedArea = maxHeight * wordInLetters.length;
+    // return highlightedArea;
+
+    // todo easier solution
+
+    int maxHeight = 0;
+
+    for (var i = 0; i < word.length; i++) {
+      int pos = word.codeUnitAt(i) - "a".codeUnitAt(0);
+
+      maxHeight = h[pos] > maxHeight ? h[pos] : maxHeight;
+      print(pos);
     }
-    var heights = [];
-    for (var i = 0; i < indexes.length; i++) {
-      heights.add(h[indexes[i]]);
-    }
-    maxHeight = heights.reduce((curr, next) {
-      if (curr > next) {
-        return curr;
-      } else {
-        return next;
-      }
-    });
-    int highlightedArea = maxHeight * wordInLetters.length;
-    return highlightedArea;
+    return maxHeight * word.length;
   }
 }
