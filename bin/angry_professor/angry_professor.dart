c62215a223cddfe1b1a5aa01d6a5fd47a0703aa1
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-09 17:21:41 +0200

    angry prof solution

--- bin/angry_professor/angry_professor.dart
+++ bin/angry_professor/angry_professor.dart
@@ -1,10 +1,21 @@
 //
 void main(List<String> args) {
-  print(AngryProfessor.angryProfessor(3, [-1, -3, 4, 2]));
+  print(AngryProfessor.angryProfessor(2, [-1, 0, 2, 1]));
 }
 
 class AngryProfessor {
+  /// k ==> the threshold (no of min student that must attend to continue)
   static String angryProfessor(int k, List<int> a) {
-    return "";
+    int noOfStudentOnTimeCounter = 0;
+    for (var i = 0; i < a.length; i++) {
+      if (a[i] <= 0) {
+        noOfStudentOnTimeCounter++;
+      }
+    }
+    if (noOfStudentOnTimeCounter < k) {
+      return "YES";
+    } else {
+      return "NO";
+    }
   }
 }
