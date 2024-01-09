8edd2834fba24ab1d52477834b30653684e6c8e9
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-09 17:12:58 +0200

    angry prof problem

---
+++ bin/angry_professor/angry_professor.dart
@@ -0,0 +1,10 @@
+//
+void main(List<String> args) {
+  print(AngryProfessor.angryProfessor(3, [-1, -3, 4, 2]));
+}
+
+class AngryProfessor {
+  static String angryProfessor(int k, List<int> a) {
+    return "";
+  }
+}
