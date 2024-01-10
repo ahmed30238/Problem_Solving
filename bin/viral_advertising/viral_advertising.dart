5bedc2f0dae6707b4b536ee20fafe81ba4642fee
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-10 14:01:00 +0200

    viral advertising solution

--- bin/viral_advertising/viral_advertising.dart
+++ bin/viral_advertising/viral_advertising.dart
@@ -1,16 +1,23 @@
 //
 void main(List<String> args) {
-  print(ViralAdvertising.viralAdvertising(5));
+  print(ViralAdvertising.viralAdvertising(3));
 }
 
 class ViralAdvertising {
   // n ==> No. of days
   static int viralAdvertising(int n) {
-    int cumulativeLikes = 0;
-    int noOfShare = 5 ~/ 2;
-    print(noOfShare);
-    for (var i = 1; i <= n; i++) {}
-    return cumulativeLikes;
+    int initShared = 5; // 1st day share ==> 5
+    int liked = 0;
+    int resultedShares = 0;
+    int cumulative = 0;
+    for (var i = 1; i <= n; i++) {
+      liked = initShared ~/ 2;
+      resultedShares = liked * 3;
+      cumulative += liked;
+      initShared = resultedShares;
+    }
+    return cumulative;
   }
 }
+
 /// اول يوم نشير مع 5 يعجبهم 2 ف كل واحد يشير مع 3
