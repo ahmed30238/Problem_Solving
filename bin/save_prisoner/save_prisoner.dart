e82bca0a171f072c54b78d67f013d2594cb40358
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-11 00:02:07 +0200

    save prisoner problem

---
+++ bin/save_prisoner/save_prisoner.dart
@@ -0,0 +1,22 @@
+//
+void main(List<String> args) {
+  print(SaveThePrisoner.saveThePrisoner(4, 6, 2));
+}
+
+class SaveThePrisoner {
+  /// n ==> no of prisoners
+  /// m ==> candies
+  /// s ==> init chair
+  static int saveThePrisoner(int n, int m, int s) {
+    
+  }
+}
+/**
+ * A jail has a number of prisoners and a number of treats to pass out to them. Their jailer decides the fairest way to divide the treats is to seat the prisoners around a circular table in sequentially numbered chairs.
+ *  A chair number will be drawn from a hat. Beginning with the prisoner in that chair, one candy will be handed to each prisoner sequentially around the table until all have been distributed.
+
+The jailer is playing a little joke, though. The last piece of candy looks like all the others, but it tastes awful.
+Determine the chair number occupied by the prisoner who will receive that candy.
+
+
+ */
