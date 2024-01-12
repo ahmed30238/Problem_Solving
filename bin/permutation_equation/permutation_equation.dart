dc7859658154bc5d4f3515f867f5a2137547587f
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-12 05:57:37 +0200

    permutation equation solution

--- bin/permutation_equation/permutation_equation.dart
+++ bin/permutation_equation/permutation_equation.dart
@@ -1,6 +1,19 @@
+void main(List<String> args) {
+  print(PermutationEquation.permutationEquation([4, 3, 5, 1, 2]));
+}
+
 class PermutationEquation {
   static List<int> permutationEquation(List<int> p) {
     List<int> res = [];
+    List<int> indicesOfxValues = [];
+    for (var x = 1; x <= p.length; x++) {
+      int index = p.indexWhere((element) => element == x);
+      indicesOfxValues.add(index + 1);
+      int index1 =
+          p.indexWhere((element) => element == indicesOfxValues[x - 1]);
+      res.add(index1 + 1);
+    }
+
     return res;
   }
 }
