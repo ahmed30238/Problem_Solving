5992a25a6fdbe5ab7ae857e959b0af7fa5c86d00
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-13 23:38:41 +0200

    finding digit solution

--- bin/finding_digits/finding_digits.dart
+++ bin/finding_digits/finding_digits.dart
@@ -4,6 +4,15 @@ void main(List<String> args) {
 
 class FindingDigits {
   static int findDigits(int n) {
-
+    int counter = 0;
+    var r = n.toString().split("").map((e) => int.parse(e)).toList();
+    for (var i = 0; i < r.length; i++) {
+      if (r[i] != 0) {
+        if (n % r[i] == 0) {
+          counter++;
+        }
+      }
+    }
+    return counter;
   }
 }
