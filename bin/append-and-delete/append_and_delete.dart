b16d1b2dea69b3b834ee68242c87bc9bb148b836
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-16 01:15:59 +0200

    append-and-delete problem

---
+++ bin/append-and-delete/append_and_delete.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  print(AppendAndDelete.appendAndDelete("aba", "aba", 7));
+}
+
+class AppendAndDelete {
+  static String appendAndDelete(String s, String t, int k) {
+    return "";
+  }
+}
