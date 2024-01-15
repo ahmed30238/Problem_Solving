f4625066e783279450029917ebff1bd011cb46f6
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-15 23:32:17 +0200

    sherlock-and-squares problem

---
+++ bin/sherlock-and-squares/sherlock_and_squares.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+void main(List<String> args) {
+  print(SherlockAndSquares.squares(3, 9));
+}
+
+class SherlockAndSquares {
+  static int squares(int a, int b) {
+  
+    // Write your code here
+  }
+}
+/**
+ * Watson likes to challenge Sherlock's math ability. He will provide a starting and ending value that describe a range of integers, inclusive of the endpoints. Sherlock must determine the number of square integers within that range.
+ */
