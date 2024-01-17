823009cf8a7187913b17e10513f73f1db3a2746b
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-17 02:28:59 +0200

    append and delete solution

--- bin/append-and-delete/append_and_delete.dart
+++ bin/append-and-delete/append_and_delete.dart
@@ -1,9 +1,22 @@
+import 'dart:math';
+
 void main(List<String> args) {
-  print(AppendAndDelete.appendAndDelete("aba", "aba", 7));
+  print(AppendAndDelete.appendAndDelete("ashley", "ash", 2));
 }
 
 class AppendAndDelete {
   static String appendAndDelete(String s, String t, int k) {
-    return "";
+    if (k >= s.length + t.length) return "Yes";
+    int same = min(s.length, t.length);
+    for (var i = 0; i < min(s.length, t.length); i++) {
+      if (s[i] != t[i]) {
+        print(i);
+        same = i;
+        break;
+      }
+    }
+    k -= s.length - same;
+    k -= t.length - same;
+    return k >= 0 && k % 2 == 0 ? "Yes" : "No";
   }
 }
