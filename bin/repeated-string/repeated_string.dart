4f6d2cfbb76cc1097495c29e04b0da92cf6a9734
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-17 03:23:39 +0200

    Repeated string problem

---
+++ bin/repeated-string/repeated_string.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  print(RepeatedString.repeatedString("aba", BigInt.parse("10")));
+}
+
+class RepeatedString {
+  static BigInt repeatedString(String s, BigInt n) {
+ 
+  }
+}
