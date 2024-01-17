6083af2fa0c96412ce9b3f0ff8aecd38cea93e35
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-17 03:53:42 +0200

    repeated string solution

--- bin/repeated-string/repeated_string.dart
+++ bin/repeated-string/repeated_string.dart
@@ -1,9 +1,22 @@
 void main(List<String> args) {
-  print(RepeatedString.repeatedString("aba", BigInt.parse("10")));
+  print(RepeatedString.repeatedString("aba", BigInt.from(10)));
 }
 
 class RepeatedString {
   static BigInt repeatedString(String s, BigInt n) {
- 
+    BigInt strLen = BigInt.from(s.length);
+    BigInt countAInS = BigInt.from(s.split('a').length - 1); 
+
+    BigInt repeats = n ~/ strLen;
+    BigInt remainder = n % strLen;
+
+    BigInt totalCount = countAInS * repeats;
+
+    String substring = s.substring(0, remainder.toInt());
+    BigInt additionalCount = BigInt.from(substring.split('a').length - 1);
+
+    totalCount += additionalCount;
+
+    return totalCount;
   }
 }
