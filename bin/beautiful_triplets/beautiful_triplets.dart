396306c00cb7983d6ae9adf3a1920b32c9fd7a1f
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-22 12:34:27 +0200

    Beautiful Triplets solution

--- bin/beautiful_triplets/beautiful_triplets.dart
+++ bin/beautiful_triplets/beautiful_triplets.dart
@@ -1,6 +1,36 @@
 void main(List<String> args) {
+  print(BeautifulTriplets.beautifulTriplets(1, [2, 2, 3, 4, 5]));
 }
 
 class BeautifulTriplets {
+  static int beautifulTriplets(int d, List<int> arr) {
+    int tripletCounter = 0;
+    for (var i = 0; i < arr.length; i++) {
+      int x = arr[i] + d;
+      if (arr.contains(arr[i] + d) && arr.contains(x + d)) {
+        tripletCounter++;
+      }
+    }
 
+    //! this is a timeout solution (not prefer)
+    ///    arr.sort((a, b) => b.compareTo(a));
+    // for (var i = 0; i < arr.length; i++) {
+    //   int x = arr[i] + d;
+    //   if (arr.contains(x) && arr.contains(x + d)) {
+    //     tripletCounter++;
+    //   }
+    // int a = i;
+    // while (a < arr.length) {
+    //   int k = i;
+    //   while (k < arr.length) {
+    //     if (arr[i] - arr[a] == d && arr[a] - arr[k] == d) {
+    //       tripletCounter++;
+    //     }
+    //     k++;
+    //   }
+    //   a++;
+    // }
+    // }
+    return tripletCounter;
+  }
 }
