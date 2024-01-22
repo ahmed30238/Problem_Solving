871c3408b6a6a806b366274b0da9d93f4d6699a1
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-22 11:39:30 +0200

    Equality In Array solution

--- bin/equality-in-a-array/equality_in_array.dart
+++ bin/equality-in-a-array/equality_in_array.dart
@@ -1,6 +1,27 @@
 void main(List<String> args) {
+  print(EqualityInArray.equalizeArray([10, 27, 9, 10, 100, 38, 30, 32, 45, 29, 27, 29, 32, 38, 32, 38, 14, 38, 29, 30, 63, 29, 63, 91, 54, 10, 63]));
 }
 
 class EqualityInArray {
-
+  static int equalizeArray(List<int> arr) {
+    Map<int, int> map = {};
+    int noOfDeletion = 0;
+    for (var item in arr) {
+      map.update(item, (value) => value + 1, ifAbsent: () => 1);
+    }
+    int maxVal = 0;
+    int maxKey = 0;
+    map.forEach((key, value) {
+      if (value > maxVal) {
+        maxVal = value;
+        maxKey = key;
+      }
+    });
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] != maxKey) {
+        noOfDeletion++;
+      }
+    }
+    return noOfDeletion;
+  }
 }
