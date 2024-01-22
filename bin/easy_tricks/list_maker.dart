c0752a41d8bc875153320bdd53bb643980e5944d
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-22 11:38:59 +0200

    EqualityInArray problem

--- bin/easy_tricks/list_maker.dart
+++ bin/easy_tricks/list_maker.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  String input = "0 0 1 0 0 1 0";
+  String input = "10 27 9 10 100 38 30 32 45 29 27 29 32 38 32 38 14 38 29 30 63 29 63 91 54 10 63";
   var c = input.split(" ").map((e) => int.parse(e)).toList();
   print(c);
 }
