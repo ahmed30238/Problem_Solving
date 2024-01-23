dd7ca542dc17d6d7fd7bf5668a6192c1ef52aa7b
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-23 06:47:22 +0200

    encryption solution

--- bin/encryption/encryption.dart
+++ bin/encryption/encryption.dart
@@ -1,10 +1,31 @@
 import 'dart:math';
 
 void main(List<String> args) {
+  print(Encryption.encryption("haveaniceday"));
 }
 
+/// have
+/// anic
+/// eday
 class Encryption {
-  
+  static String encryption(String s) {
+    String input = s.replaceAll(" ", "");
+    int l = input.length;
+    int row = sqrt(l).floor();
+    int columns = sqrt(l).ceil();
+    List<String> grid = List.generate(columns, (index) => "");
+    if (row * columns > l) {
+      row++;
+    }
+    for (var i = 0; i < l; i++) {
+      grid[i % columns] += input[i];
+      print(i % columns);
+    }
+
+
+    var res = grid.join(" ");
+    return res;
+  }
 }
 /**
  An English text needs to be encrypted using the following encryption scheme.
