f7b8991493a4d84cb1349916583c5d1421aeaf13
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-01-23 06:21:10 +0200

    encryption problem

---
+++ bin/encryption/encryption.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+void main(List<String> args) {
+}
+
+class Encryption {
+  
+}
+/**
+ An English text needs to be encrypted using the following encryption scheme.
+ First, the spaces are removed from the text. Let  be the length of this text.
+ Then, characters are written into a grid, whose rows and columns have the following constraints:
+
+
+//!
+// After removing spaces, the string is 54 characters long.
+// sqrt(54) is between 7 and 8, so it is written in the form of a grid with 7 rows and 8 columns.
+ */
