9e3eecff0d9ff9ea8167673261cc2c0d0392f1f8
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2024-09-12 18:10:43 +0300

    lisa's workbook problem

---
+++ bin/lisa's_workbook/lisa's_workbook.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  print(LisaWorkBook.workbook(5, 3, [4, 2, 6, 1, 10]));
+}
+
+class LisaWorkBook {
+  static int workbook(int n, int k, List<int> arr) {
+    // n ==> number of chapthers
+    // k ==> ni of problem in one page
+    // arr[i] ==> no of problem in this chapter
+    return 0;
+  }
+}
